08406567d9ee1a04b39136ba0bd932d01d60ad56
muhamadnabila <mbilalghz.gmail.com>
2019-06-27 21:20:17 +0700

    add room

--- src/store.js
+++ src/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+      isLogin : false
   },
   mutations: {
-
+    login(state, payload) {
+      state.isLogin = payload
+    }
   },
   actions: {
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="home">
-   <button>tekaan woi</button>
+    <div v-if="$store.state.isLogin == false">
+      <label for="">Your Name</label>
+      <br>
+      <input v-model="username" type="text">
+      <button @click="login">Submit</button>
+    </div>
+    <div v-if="$store.state.isLogin" class="main">
+      <h1>Home</h1>
+      <div class="create-room">
+        <form @submit.prevent="createRoom" action="">
+          <h1>Create Room</h1>
+          <br>
+          <label for="">Room Name</label>
+          <br>
+          <input v-model="roomName" type="text">
+          <br>
+          <button type="submite">Create</button>
+        </form>
+      </div>
+      <div class="list-room">
+        <ul>
+          <li v-for="(room, index) in roomList" :key="index">
+              <h2>
+                {{ room.name }}
+              </h2>
+              <button @click="joinRoom(room.idRoom)" >Join</button>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,39 +42,76 @@ import { constants } from 'crypto';
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      username: '',
+      roomName: '',
+      roomList: []
+    }
   },
   methods: {
+    joinRoom (idRoom) {
+      db.collection('room').doc(idRoom).get()
+        .then(doc => {
+          let newRoom = {
+            name: doc.data().name,
+            players: doc.data().players,
+          }
+          newRoom.players.push({
+            name: localStorage.getItem('username'),
+            position: 0
+          })
+          return db.collection('room').doc(idRoom).set({
+            ...newRoom
+          }, { merge: true })
+        })
+        .then(doc => {
+          this.$router.push(`/lobby/${idRoom}`)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    login() {
+      this.$store.commit('login',true)
+      localStorage.setItem('username',this.username)
+    },
+    createRoom () {
+      let username = localStorage.getItem('username')
+      db.collection("room").add({
+        name: this.roomName,
+        roomMater: username,
+        players : [{
+          name: username,
+          position: 0,
+        }]
+      })
+      .then(function(docRef) {
+        console.log('berhasil')
+          console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.log('error')
+          console.error("Error adding document: ", error);
+      })
+    }
   },
   created () {
-    console.log('masuk sini')
-    db.collection("room").add({
-      name: "Tokyo",
-      country: "Japan"
-    })
-    .then(function(docRef) {
-      console.log('berhasil')
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.log('error')
-        console.error("Error adding document: ", error);
-    });
-    var docRef = db.collection("room")
-    docRef.get().then(function(doc) {
-      doc.forEach(element => {
-        console.log(element.data())
-      });
-      // console.log("Document data:", doc.data());
-    if (doc.exists) {
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
+    if(localStorage.getItem('username')){
+      this.$store.commit('login',true)
+    }else {
+      this.$store.commit('login',false)
     }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
+    db.collection("room")
+    .onSnapshot((querySnapshot) => {
+          let rooms = []
+        querySnapshot.forEach((doc) => {
+            let idRoom = doc.id
+            rooms.push({ ...doc.data(), ...{idRoom} })
+        });
+        this.roomList = rooms
     });
-
   }
 }
 </script>
